ea6ee1241d75c17ead65c147ddccf20f68b70325
Esrat <imranzaman.318@gmail.com>
2021-03-13 20:38:54 -0500

    Completed Done

---
+++ index.js
@@ -0,0 +1,18 @@
+ function introduction(name) {
+        return `Hi, my name is ${name}.`;
+}
+console.log('Josh');
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log('Josh','Ember.js');
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language} .`;
+}
+console.log("Gracie","JavaScript");
+
+function introductionWithLanguageOptional(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log("Gracie","Python");
